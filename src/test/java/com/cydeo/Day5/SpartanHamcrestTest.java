11008070f571dcbd4e4ba831f094e298111dc9be
erenkocyigit <m.erenkocyigit@gmail.com>
2022-07-19 15:10:44 +0200

    extrat method

---
+++ src/test/java/com/cydeo/Day5/SpartanHamcrestTest.java
@@ -0,0 +1,56 @@
+package com.cydeo.Day5;
+
+
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class SpartanHamcrestTest extends SpartanTestBase {
+
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test1(){
+
+        List<String> names = given().accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains","j",
+                        "gender","Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("totalElement",greaterThanOrEqualTo(3))
+                .extract().response().jsonPath().getList("content.name");
+
+        System.out.println(names);
+    }
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test2(){
+
+        int statusCode = given().accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains","j",
+                        "gender","Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("totalElement",greaterThanOrEqualTo(3))
+                .extract().response().statusCode();
+
+        System.out.println(statusCode);
+    }
+
+
+
+}
