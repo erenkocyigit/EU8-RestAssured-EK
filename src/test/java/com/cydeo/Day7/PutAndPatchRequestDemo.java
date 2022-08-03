015fb728634f5a0215423d7d6fb7af6c1412e285
erenkocyigit <m.erenkocyigit@gmail.com>
2022-08-03 13:14:59 +0200

    day10 contenttype xml

--- src/test/java/com/cydeo/Day7/PutAndPatchRequestDemo.java
+++ src/test/java/com/cydeo/Day7/PutAndPatchRequestDemo.java
@@ -47,6 +47,40 @@ public class PutAndPatchRequestDemo extends SpartanTestBase {
 
     }
 
+    @DisplayName("PATCH request to one spartan for partial update with Map")
+    @Test
+    public void PATCHRequest(){
+        //just like post request we have different options to send body, we will go with map
+        Map<String,Object> putRequestMap = new LinkedHashMap<>();
+        putRequestMap.put("phone",8811111111L);
+        putRequestMap.put("name","Peter");
+
+        given().contentType(ContentType.JSON) //hey api I am sending JSON body
+                .body(putRequestMap).log().body()
+                .and().pathParam("id",110)
+                .when().patch("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
+
+        //send a GET request after update, make sure updated field changed, or the new info matching
+        //with requestBody that we send
+
+
+    }
+    @DisplayName("DELETE one spartan")
+    @Test
+    public void deleteSpartan(){
+        int idToDelete= 110;
+
+
+        given().pathParam("id",idToDelete)
+                .when().delete("/api/spartans/{id}")
+                .then().statusCode(204);
+
+        //send a get request after you delete make sure you are getting 404
+
+    }
+
 
 
 
