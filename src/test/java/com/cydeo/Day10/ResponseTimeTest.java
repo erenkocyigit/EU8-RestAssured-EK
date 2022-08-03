2c2bff2e7e258bab006e7dd8ead43b8cc256a5c3
erenkocyigit <m.erenkocyigit@gmail.com>
2022-08-03 19:54:34 +0200

    day11 junit5 updates

--- src/test/java/com/cydeo/Day10/ResponseTimeTest.java
+++ src/test/java/com/cydeo/Day10/ResponseTimeTest.java
@@ -1,4 +1,27 @@
 package com.cydeo.Day10;
 
-public class ResponseTimeTest {
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ResponseTimeTest extends SpartanAuthTestBase {
+
+    @Test
+    public void test1() {
+
+        Response response = given().auth().basic("admin", "admin")
+                .accept(ContentType.JSON).when()
+                .get("/api/spartans")
+                .then().time(both(greaterThan(500L)).and(lessThan(1100l))).extract().response();
+
+
+        System.out.println("response = " + response.getTime());
+
+    }
 }
