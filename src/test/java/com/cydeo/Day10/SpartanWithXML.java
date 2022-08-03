b49a2616a6568a832940c66ea5674f270b549307
erenkocyigit <m.erenkocyigit@gmail.com>
2022-08-03 16:15:33 +0200

    resources file added

--- src/test/java/com/cydeo/Day10/SpartanWithXML.java
+++ src/test/java/com/cydeo/Day10/SpartanWithXML.java
@@ -2,8 +2,12 @@ package com.cydeo.Day10;
 
 import com.cydeo.utilities.SpartanAuthTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,7 +28,26 @@ public class SpartanWithXML extends SpartanAuthTestBase {
                 .body("List.item[0].name",is("Meade"))
                 .body("List.item[0].gender",is("Male"))
                 .log().all();
+    }
 
+    @DisplayName("GET request /api/spartans with xmlPath")
+    @Test
+    public void testXmlPath(){
+        Response response = given().accept(ContentType.XML).and()
+                .auth().basic("admin", "admin").when()
+                .get("/api/spartans");
+
+        XmlPath xmlPath = response.xmlPath();
+
+        String name = xmlPath.get("List.item[0].name");
+        System.out.println("name = " + name);
+
+        int id = xmlPath.getInt("List.item[2].id");
+        System.out.println("id = " + id);
+
+        //how to get all names and save into list of string
+        List<String> names = xmlPath.getList("List.item.name");
+        System.out.println(names);
 
     }
 
